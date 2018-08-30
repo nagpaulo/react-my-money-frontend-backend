399ca96aa7fd28dac7b2c3d4ef0c508774c26c80
nagpaulo <nagpaulo@gmail.com>
2018-08-29 21:09:27 -0300

    Template - Menu

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,11 +1,17 @@
 import '../common/template/dependencias'
 import React from 'react';
 import Header from '../common/template/header';
-import SideBar from '../common/template/sidebar'
+import SideBar from '../common/template/sidebar';
+import Footer from '../common/template/footer';
+import Routes from './routes';
 
 export default props => (
     <div className="wrapper">
         <Header />
         <SideBar />
+        <div className="content-wrapper">
+            <Routes />
+        </div>
+        <Footer />
     </div>
 )
